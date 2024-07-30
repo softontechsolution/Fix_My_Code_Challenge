3407af8bde804fbbed4466e73bd2de8f9e205bde
softontechsolution <softontechsolution@gmail.com>
2024-07-30 09:31:56 +0000

    user fixed

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -9,6 +9,11 @@ class User():
     def __init__(self):
         """ Documentation """
         self.__email = None
+    
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -16,11 +21,6 @@ class User():
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
    
     
 if __name__ == "__main__":
