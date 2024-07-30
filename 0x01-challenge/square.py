a1df4477bf861559d9af22440396b75de3282496
softontechsolution <softontechsolution@gmail.com>
2024-07-30 09:15:47 +0000

    square fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Square Module"""
 
-class square():
+
+class Square():
     
     width = 0
     height = 0
@@ -12,9 +14,9 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -22,7 +24,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
